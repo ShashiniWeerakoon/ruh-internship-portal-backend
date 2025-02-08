94c0e6efdb9b5a2340a72fe7ba7d07ba5b20eb04
ShashiniWeerakoon <150053909+ShashiniWeerakoon@users.noreply.github.com>
2025-02-08 10:26:18 +0530

    first commit

---
+++ ruh-internship-portal-backend/src/main/java/ruh_internship_portal_backend/example/ruh_internship_portal_backend/RuhInternshipPortalBackendApplication.java
@@ -0,0 +1,13 @@
+package ruh_internship_portal_backend.example.ruh_internship_portal_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RuhInternshipPortalBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RuhInternshipPortalBackendApplication.class, args);
+	}
+
+}
---
+++ ruh-internship-portal-backend/src/test/java/ruh_internship_portal_backend/example/ruh_internship_portal_backend/RuhInternshipPortalBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package ruh_internship_portal_backend.example.ruh_internship_portal_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RuhInternshipPortalBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
