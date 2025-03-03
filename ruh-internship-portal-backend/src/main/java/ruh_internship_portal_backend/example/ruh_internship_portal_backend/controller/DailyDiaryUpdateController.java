5821086235908154a6a84d736d18b8e6bfa0c1b1
Thejana Hirumal <thejanahirumal5@gmail.com>
2025-03-03 10:44:33 +0530

    updated WeeklyDiaryUpdate

--- ruh-internship-portal-backend/src/main/java/ruh_internship_portal_backend/example/ruh_internship_portal_backend/controller/DailyDiaryUpdateController.java
+++ ruh-internship-portal-backend/src/main/java/ruh_internship_portal_backend/example/ruh_internship_portal_backend/controller/DailyDiaryUpdateController.java
@@ -1,23 +1,20 @@
 package ruh_internship_portal_backend.example.ruh_internship_portal_backend.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import ruh_internship_portal_backend.example.ruh_internship_portal_backend.dto.DailyDiaryUpdateDTO;
 import ruh_internship_portal_backend.example.ruh_internship_portal_backend.service.DailyDiaryUpdateService;
 
 
 @RestController
 @CrossOrigin
-@RequestMapping("/api/dailyDiaryUpdate")
+@RequestMapping("/api/diaryUpdate/daily")
 public class DailyDiaryUpdateController {
 
     @Autowired
     private DailyDiaryUpdateService dailyDiaryUpdateService;
 
-    @RequestMapping("/save")
+    @PostMapping("/save")
     public String saveDailyDiaryUpdate(@RequestBody DailyDiaryUpdateDTO dailyDiaryUpdateDTO) {
         dailyDiaryUpdateService.saveDailyDiaryUpdate(dailyDiaryUpdateDTO);
         return "save";
--- ruh-internship-portal-backend/src/main/java/ruh_internship_portal_backend/example/ruh_internship_portal_backend/controller/FeedbackController.java
+++ ruh-internship-portal-backend/src/main/java/ruh_internship_portal_backend/example/ruh_internship_portal_backend/controller/FeedbackController.java
@@ -1,10 +1,7 @@
 package ruh_internship_portal_backend.example.ruh_internship_portal_backend.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import ruh_internship_portal_backend.example.ruh_internship_portal_backend.dto.FeedbackDTO;
 import ruh_internship_portal_backend.example.ruh_internship_portal_backend.service.FeedbackService;
 
@@ -15,7 +12,7 @@ public class FeedbackController {
     @Autowired
     private FeedbackService feedbackService;
 
-    @RequestMapping("/save")
+    @PostMapping("/save")
     public String saveFeedback(@RequestBody FeedbackDTO feedbackDTO) {
         feedbackService.saveFeedback(feedbackDTO);
         return "save";
